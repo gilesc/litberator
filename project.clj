f0e63dc46fd7caf36eed8f19bba01d6fd2ce2030
Cory Giles <cory.b.giles@gmail.com>
2012-01-17 16:25:09 -0600

    fix version number in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject litberator "1.0.1         "
+(defproject litberator "1.0.1"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
